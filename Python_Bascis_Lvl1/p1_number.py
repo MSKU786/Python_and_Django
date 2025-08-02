79a201d653b24a9f128d7291a108d493d437de6d
MSKU786 <manishsingh332000@gmail.com>
2025-08-02 17:05:10 +0530

    Learning Python

---
+++ Python_Bascis_Lvl1/p1_number.py
@@ -0,0 +1,12 @@
+print(2+10 *  3+10);
+print((2+10) * (3+10));
+a = 5
+print(a+a)
+a = a*5
+print(a)
+
+#python variable name can't start with number
+my_income = 100
+my_tax_rate = 0.1
+my_taxes = my_income * my_tax_rate
+print(my_taxes)
---
+++ Python_Bascis_Lvl1/p2_strings.py
@@ -0,0 +1,24 @@
+#Baics
+
+# 'hello'
+# "hello"
+# "I'am an engineer"
+
+my_string = "Hello World"
+
+#slice
+print(my_string)
+print(my_string[2])
+print(my_string[2:])
+print(my_string[:3])
+print(my_string[::2])
+print(my_string.upper());
+print(my_string.lower());
+print(my_string.split());
+
+#string are immutable 
+
+#formatting
+print("This is a string {}".format("INSERTED"))
+print("The {} {} {}".format("fox", "brown", "quick"))
+print("The {2} {1} {0}".format("fox", "brown", "quick"))
