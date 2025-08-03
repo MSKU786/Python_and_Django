487967b92a00d9ad5472188eecc7104f01b3bb25
MSKU786 <manishsingh332000@gmail.com>
2025-08-03 15:48:35 +0530

    Part 4 Dictionaries

---
+++ Python_Bascis_Lvl1/p4_dictionaries.py
@@ -0,0 +1,8 @@
+my_dict = {"key1": 13, "key2": 'value2', "key3": {'123': [1,2,3]}};
+print(my_dict);
+print(my_dict["key3"]['123'][1]);
+
+my_dict['key4'] = True;
+
+my_dict['key2'] = 'updated value2'
+print(my_dict);
