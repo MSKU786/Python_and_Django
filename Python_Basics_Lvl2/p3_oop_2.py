c0b1b7118fa60efc3c91f66ae6895577c7378140
MSKU786 <manishsingh332000@gmail.com>
2025-08-13 10:26:33 +0530

    Added speicial methods for classes

--- Python_Basics_Lvl2/p3_oop_2.py
+++ Python_Basics_Lvl2/p3_oop_2.py
@@ -35,3 +35,27 @@ myDog.eat();
 
 # Special Methods
 
+class Book():
+
+  def __init__(self, title, author, pages):
+    self.title = title
+    self.author = author
+    self.pages = pages
+
+  def __str__(self):
+    return "Title: {}, Author: {}, Page: {}".format(self.title, self.author, self.pages)
+  
+  def __len__(self):
+    return self.pages;
+
+  def __del__(self):
+    print("Book has been destroy")
+
+book = Book("Python", "Manish", 200)
+print(book);
+print(len(book));
+del book;
+
+my_list = [1,2,3,4]
+print(my_list)
+
