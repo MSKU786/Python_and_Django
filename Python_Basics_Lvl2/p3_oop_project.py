508c44c6f3bb2546dd6304f482e7e87f8557ecf8
MSKU786 <manishsingh332000@gmail.com>
2025-08-15 17:38:34 +0530

    Completed Deck class with shuffle and split

--- Python_Basics_Lvl2/p3_oop_project.py
+++ Python_Basics_Lvl2/p3_oop_project.py
@@ -9,7 +9,21 @@ class Deck:
   """
     This is Deck Class. The object will create a deck of cards to initate play. You can then use this Deck list of artds to split in half and give to the player.s It will use SUITE and RANKS to create the dec. It should aslo have a methods for spllitting the deck in half and shuffling the deck.
   """
-  pass
+
+  def __init__(self):
+    self.cards = []
+    for s in SUITE:
+      for r in RANKS:
+        self.cards.append(s+'_'+r)
+    print(self.cards)
+  
+  def shuffleDeck(self):
+    return shuffle(self.cards)
+  
+  def split(self):
+    len_cards = len(self.cards)
+    return self.cards[:len_cards//2], self.cards[len_cards//2:]
+
 
 class Hand:
   """
@@ -21,3 +35,9 @@ class Player:
   """
   This is the player class, whichc takes ihna anme an instance of a Hand class object. The player can they play cards and check if they still have cards. 
   """
+
+deck = Deck()
+deck.shuffleDeck()
+p1,p2 = deck.split()
+print(p1)
+print(p2)
