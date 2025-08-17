2ee812475c470291c0cb9ff10c5df7c17c122278
MSKU786 <manishsingh332000@gmail.com>
2025-08-17 07:08:57 +0530

    Error and expection handling

--- Python_Basics_Lvl2/p4_error_and_excpetion.py
+++ Python_Basics_Lvl2/p4_error_and_excpetion.py
@@ -12,7 +12,7 @@ print("other variation")
 try:
   f = open('simple.txt','r')
   f.write("Test write to simple text!")
-except IOError:
+except:
   print('ERROR! COULD NOT FIND FILE OR READ DATA')
 finally:
   print("FINAL BLOCK WILL ALWAYS WORK NO MATTER WHAT")
