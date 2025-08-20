66f43d1d3fe4d3428904d0497b88aa7780cb7a1d
MSKU786 <manishsingh332000@gmail.com>
2025-08-20 10:58:57 +0530

    Example of returing a function from function

--- Python_Basics_Lvl2/p7_Decorators/scopes.py
+++ Python_Basics_Lvl2/p7_Decorators/scopes.py
@@ -9,3 +9,37 @@ def func():
   print(globals())
 
 func()
+
+
+# Assign funciton to a variable
+
+def hello(name = 'Ajay'):
+  return 'Helloooo.... '+ name
+
+print(hello())
+
+my_new_greet = hello
+print(my_new_greet())
+
+
+
+def outer_function(param = 'first'):
+  print("The outer function has been revoked")
+  def inner():
+    print("I am inside inner function")
+  def inner2():
+    print("I am inside inner 2 function")
+  
+  if param == 'first':
+    return inner
+  else
+    return inner2
+  print("End of outer function")
+
+# if you try to call this directly it will throw error
+#inner()
+
+
+x = outer_function();
+
+print(x())
