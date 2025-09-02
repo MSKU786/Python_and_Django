da8e60f767106e959345fe74c2b096115e4f7873
MSKU786 <manishsingh332000@gmail.com>
2025-09-02 10:23:27 +0530

    Added new task for templates

--- Django_Basics_Lvl1/Lecture2_Task/task_project/AppTwo/views.py
+++ Django_Basics_Lvl1/Lecture2_Task/task_project/AppTwo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(req):
-  return HttpResponse('<em>My Second App</em>')
+def index(request):
+  my_dict = {"insert_me": "This is from my help page"}
+
+  return render(request, 'index.html', context=my_dict)
