d3899ceb0c10216b2b2a7c180a936a5d1d6151a5
MSKU786 <manishsingh332000@gmail.com>
2025-09-02 10:26:12 +0530

    Minor correection

--- Django_Basics_Lvl1/Lecture2_Task/task_project/AppTwo/views.py
+++ Django_Basics_Lvl1/Lecture2_Task/task_project/AppTwo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
+def index(req):
+  return HttpResponse('<em>My Second App</em>')
+
+
+def help(request):
   my_dict = {"insert_me": "This is from my help page"}
 
-  return render(request, 'index.html', context=my_dict)
+  return render(request, 'apptwo/help.html', context=my_dict)
