a13c3e9850ab12922f8e98b1bcf279dfcee28115
MSKU786 <manishsingh332000@gmail.com>
2025-09-18 10:23:34 +0530

    Added cuostmer validator using clean

--- Django_Basics_LVL3/basicforms/basicapp/forms.py
+++ Django_Basics_LVL3/basicforms/basicapp/forms.py
@@ -4,4 +4,14 @@ class FormName(forms.Form):
   name = forms.CharField()
   email = forms.EmailField()
   text = forms.CharField(widget=forms.Textarea)
+  bot_catcher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+
+  def clean_bot_catcher(self):
+    botcatcher = self.cleaned_data['bot_catcher']
+    if (len(botcatcher) > 0):
+      raise forms.ValidationError("Gotcha Bot!!!")
+    return botcatcher
+  
+  
   
