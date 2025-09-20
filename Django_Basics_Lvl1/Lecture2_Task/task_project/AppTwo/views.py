1e4288d3825730fb3f0176817bb810af21bccb7c
MSKU786 <manishsingh332000@gmail.com>
2025-09-20 11:53:33 +0530

    Added form request over the views

--- Django_Basics_Lvl1/Lecture2_Task/task_project/AppTwo/views.py
+++ Django_Basics_Lvl1/Lecture2_Task/task_project/AppTwo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from AppTwo.models import User
+fromm AppTwo.forms import NewUserForm
 # Create your views here.
 def index(req):
   return HttpResponse('<em>My Second App</em>')
@@ -12,6 +13,18 @@ def help(request):
   return render(request, 'apptwo/help.html', context=my_dict)
 
 def users(request):
-  users_list = User.objects.order_by('email')
-  user_dict = {'users_records': users_list}
-  return render(request, 'apptwo/users.html', context=user_dict)
+  # users_list = User.objects.order_by('email')
+  # user_dict = {'users_records': users_list}
+  # return render(request, 'apptwo/users.html', context=user_dict)
+  form = NewUserForm()
+
+  if (request.method == 'POST'):
+    form = NewUserForm(request.POST)
+
+    if form.is_valid():
+      form.save(commit=True)
+      return help(request)
+    else:
+      print("Error!! form is not valid")
+  
+  return render(request, 'apptwo/users.html', {'form': form})
