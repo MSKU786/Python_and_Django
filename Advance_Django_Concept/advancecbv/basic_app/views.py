aba13bde562bb350363b01b4968a41e08b6b0c73
MSKU786 <manishsingh332000@gmail.com>
2025-10-12 16:58:08 +0530

    Added template based context views

--- Advance_Django_Concept/advancecbv/basic_app/views.py
+++ Advance_Django_Concept/advancecbv/basic_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View, Template View
+# from django.http import HttpResponse
+# # Create your views here.
+# def index(request):
+#   return render(request, 'index.html')
 
-# Create your views here.
+# class CBView(View):
+#   def get(self, request): 
+#     return HttpResponse("Class based views are hard to implement")
+
+class IndexView(TemplateView):
+  template_name = 'index.html'
+
+  def get_context_data(self, **kwargs):
+    context= super().get_context_data(**kwargs)
+    context['injectme'] = 'BASIC INJECTION'
+    return context
