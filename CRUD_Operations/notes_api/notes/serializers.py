32a940eebc2c44bfb9295ac78ade52c88f048e5a
MSKU786 <manishsingh332000@gmail.com>
2025-12-08 10:11:14 +0530

    Addedcustom validation , maniputlation and read-only fields

--- CRUD_Operations/notes_api/notes/serializers.py
+++ CRUD_Operations/notes_api/notes/serializers.py
@@ -1,7 +1,46 @@
 from rest_framework import serializers
 from .models import Note
 
+# Added custom validation , maniputlation and read-only fields
 class NoteSerializer(serializers.ModelSerializer):
+    summary = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Note
-        fields = '__all__'
+        fields = [
+            "id", "title", "content", "created_at", "updated_at",
+            "is_archived", "is_deleted", "summary"
+        ]
+        read_only_fields = ("created_at", "updated_at")
+
+        # ----- FIELD LEVEL VALIDATION -----
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Title must be at least 3 chars")
+        
+        if (len(value) > 200):
+            raise serializers.ValidationError("Title must be less than 200 chars")
+    
+        return value.strip()
+
+        # ----- OBJECT LEVEL VALIDATION -----
+    def validate(self, attrs):
+        if attrs.get("title") == attrs.get("content"):
+            raise serializers.ValidationError(
+                "Title and content cannot be identical"
+            )
+        return attrs
+
+    # ----- DATA MANIPULATION BEFORE SAVE -----
+    def create(self, validated_data):
+        validated_data["title"] = validated_data["title"].title()  # Format title
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if "title" in validated_data:
+            validated_data["title"] = validated_data["title"].title()
+        return super().update(instance, validated_data)
+
+    # ----- CUSTOM READ-ONLY FIELD -----
+    def get_summary(self, note):
+        return note.content[:50] + "..."
--- CRUD_Operations/notes_api/notes_api/urls.py
+++ CRUD_Operations/notes_api/notes_api/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from notes.views import NoteViewSet
+
+router = routers.DefaultRouter()
+router.register(r'notes', NoteViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
